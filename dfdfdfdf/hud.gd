51e447e96d9adf5bf243fcd60edcd88708d312ae
si-lyeon <166217929+si-lyeon@users.noreply.github.com>
2025-01-25 23:51:35 +0900

    Add files via upload

---
+++ dfdfdfdf/hud.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+signal start_game
+
+func update_score(value : int) -> void:
+	$MarginContainer/Score.text = str(value)
+	
+func update_timer(value : int) -> void:
+	$MarginContainer/Time.text = str(value)
+
+func show_message(text : String) -> void:
+	$Message.text = text
+	$Message.show()
+	$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	$Message.hide()
+
+
+func _on_start_button_pressed() -> void:
+	$StartButton.hide()
+	$Message.hide()
+	start_game.emit()
+
+func show_game_over() -> void:
+	show_message("Game Over")
+	await  $Timer.timeout
+	$StartButton.show()
+	$Message.text = "Coin Dash!"
+	$Message.show()
---
+++ dfdfdfdf/main.gd
@@ -0,0 +1,67 @@
+extends Node2D
+
+@export var coin_scene : PackedScene
+@export var playtime = 30
+
+var level = 1
+var score = 0
+var time_left = 0
+var screensize = Vector2.ZERO
+var playing = false
+
+func _ready() -> void:
+	screensize = get_viewport().get_visible_rect().size
+	$Player.screensize = screensize
+	$Player.hide()
+
+func _process(delta: float) -> void:
+	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
+		level += 1
+		time_left += 5
+		spawn_coins() 
+
+func spawn_coins() -> void:
+	for i in level + 4:
+		var c = coin_scene.instantiate()
+		add_child(c)
+		c.screensize = screensize
+		c.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
+
+func new_game() -> void:
+	playing = true
+	level = 1
+	score = 0
+	time_left = playtime
+	$Player.start()
+	$Player.show()
+	$GameTimer.start()
+	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
+
+
+func _on_game_timer_timeout() -> void:
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
+
+
+func _on_player_hurt() -> void:
+	game_over()
+
+
+func _on_player_pickup() -> void:
+	score += 1
+	$HUD.update_score(score)
+
+func game_over() -> void:
+	playing = false
+	$GameTimer.stop()
+	get_tree().call_group("coins", "queue_free")
+	$HUD.show_game_over()
+	$Player.die()
+
+
+func _on_hud_start_game() -> void:
+	new_game()
